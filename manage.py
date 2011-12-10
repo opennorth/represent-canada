6b36943c07189eba7dd6b6361edb81730024816b
James McKinney <james@slashpoundbang.com>
2011-12-10 01:37:48 -0500

    change manage.py to default

--- manage.py
+++ manage.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
-import os
 from django.core.management import execute_manager
-
-if not os.environ.has_key("DJANGO_SETTINGS_MODULE"):
-    if not os.environ.has_key("DEPLOYMENT_TARGET"):
-        os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
-    else:
-        os.environ["DJANGO_SETTINGS_MODULE"] = "config.%s.settings" % os.environ["DEPLOYMENT_TARGET"]
-
-settings_module = os.environ["DJANGO_SETTINGS_MODULE"]
-
+import imp
 try:
-    settings = __import__(settings_module, globals(), locals(), ['settings'], -1)
+    imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file '%s.py'.\n" % settings_module)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
+
+import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
