c449b275da42f0bf5d3842cf2c522eb951136efa
Michael Mulley <michael@michaelmulley.com>
2011-12-25 16:19:48 -0500

    We don't need tastypie in INSTALLED_APPS anymore

--- settings.py
+++ settings.py
@@ -121,7 +121,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'compressor',
-    'tastypie',
     'boundaryservice',
     'finder',
     'repapi',
