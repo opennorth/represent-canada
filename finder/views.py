ec6fc993faa3cbad355c908dc162968711e40996
Michael Mulley <michael@michaelmulley.com>
2012-01-29 01:53:36 -0500

    Update homepage; make coverage list live from DB

--- finder/views.py
+++ finder/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.conf import settings
 
-# @todo Shouldn't really be here.
-def settings_processor(request):
-    return {'settings': settings}
+from boundaries.models import BoundarySet
+try:
+    from representatives.models import RepresentativeSet
+    USE_REPS = True
+except ImportError:
+    USE_REPS = False
 
 def index(request):
-    return render_to_response('index.html', RequestContext(request))
+    sets = list(BoundarySet.objects.all().order_by('name').values('slug', 'name'))
+    set_slugs_with_reps = dict(RepresentativeSet.objects.all().values_list('boundary_set', 'slug'))
+    for s in sets:
+        s['reps'] = set_slugs_with_reps.get(s['slug'])
+    return render_to_response('index.html', RequestContext(request, {'boundary_sets': sets}))
 
 def api(request):
     return render_to_response('api.html', RequestContext(request))
