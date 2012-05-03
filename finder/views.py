2edc27e885e47c04a7bac00c422a223ff8e9f29a
James McKinney <james@slashpoundbang.com>
2012-05-03 17:02:48 -0400

    redo homepage

--- finder/views.py
+++ finder/views.py
@@ -1,19 +1,45 @@
+# coding: utf-8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from collections import defaultdict
 from boundaries.models import BoundarySet
+
 try:
-    from representatives.models import RepresentativeSet
-    USE_REPS = True
+  from representatives.models import RepresentativeSet
+  USE_REPS = True
 except ImportError:
-    USE_REPS = False
+  USE_REPS = False
 
 def index(request):
-    sets = list(BoundarySet.objects.all().order_by('name').values('slug', 'name'))
-    set_slugs_with_reps = dict(RepresentativeSet.objects.all().values_list('boundary_set', 'slug'))
-    for s in sets:
-        s['reps'] = set_slugs_with_reps.get(s['slug'])
-    return render_to_response('index.html', RequestContext(request, {'boundary_sets': sets}))
+  domain_to_category_map = {
+    'Canada'                   : 'Federal',
+    'British Columbia'         : 'Provincial',
+    'Alberta'                  : 'Provincial',
+    'Saskatchewan'             : 'Provincial',
+    'Manitoba'                 : 'Provincial',
+    'Ontario'                  : 'Provincial',
+    u'Québec'                  : 'Provincial',
+    'New Brunswick'            : 'Provincial',
+    'Prince Edward Island'     : 'Provincial',
+    'Nova Scotia'              : 'Provincial',
+    'Newfoundland and Labrador': 'Provincial',
+    'Yukon'                    : 'Territorial',
+    'Northwest Territories'    : 'Territorial',
+    'Nuvavut'                  : 'Territorial',
+  }
+
+  boundary_sets = list(BoundarySet.objects.all().order_by('name').values('slug', 'name', 'domain'))
+  representative_sets = list(RepresentativeSet.objects.all().values('slug', 'name', 'boundary_set'))
+
+  # @note Assumes boundary sets have at most one representative set.
+  representative_sets = dict((x['boundary_set'], x) for x in representative_sets if x.get('boundary_set'))
+
+  categories = defaultdict(list)
+  for boundary_set in boundary_sets:
+    boundary_set['representative_set'] = representative_sets.get(boundary_set['slug'])
+    categories[domain_to_category_map.get(boundary_set['domain'], 'Municipal')].append(boundary_set)
+
+  return render_to_response('index.html', RequestContext(request, {'categories': categories.iteritems()}))
 
 def api(request):
-    return render_to_response('api.html', RequestContext(request))
+  return render_to_response('api.html', RequestContext(request))
