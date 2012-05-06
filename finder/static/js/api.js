08ed45e1d385abbfec609f99eaee79c572806aaf
James McKinney <james@slashpoundbang.com>
2012-05-06 10:24:34 -0400

    redo design

--- finder/static/js/api.js
+++ finder/static/js/api.js
@@ -1,18 +1,60 @@
 jQuery(function ($) {
-  // Loads and displays JSON for <pre class="loadjson" data-url="/resource/">.
-  $('pre.loadjson').each(function () {
-    var $this = $(this);
-    if ($this.is(':empty')) {
-      var url = $this.data('url');
-      $this.append('<div class="url"><code>' + url + '</code></div>');
-      $.ajax({
-        url: url + (url.indexOf('?') === -1 ? '?' : '&') + 'pretty=1',
-        dataType: 'text',
-        success: function (data) {
-          $this.append(formatJSON(data.replace(/[?&]pretty=1/g, '')));
-          $('body').scrollSpy('refresh');
-        }
-      });
+  $('body').scrollspy({target: '.subnav', offset: 55}); // 1 more than full height of subnav
+
+  // http://twitter.github.com/bootstrap/assets/js/application.js
+  var $window = $(window),
+      $subnav = $('.subnav'),
+      offset = $('.subnav').length && $('.subnav').offset().top,
+      fixed = false;
+
+  function processScroll() {
+    var scrollTop = $window.scrollTop()
+    if (scrollTop >= offset && !fixed) {
+      fixed = true;
+      $subnav.addClass('subnav-fixed');
+    } else if (scrollTop <= offset && fixed) {
+      fixed = false;
+      $subnav.removeClass('subnav-fixed');
+    }
+  }
+  processScroll();
+
+  $window.on('scroll', processScroll);
+
+  $subnav.on('click', function () {
+    if (!fixed) {
+      setTimeout(function () {
+        $window.scrollTop($window.scrollTop() - 54); // full height of subnav
+      }, 10);
     }
   });
+
+  $('#apibrowser pre').html(formatJSON($('#apibrowser pre').text()));
+
+/*
+  var $modal = $('#modal');
+
+  function processModal(url) {
+    $modal.find('h3').text(url);
+    $.ajax({
+      url: url + (url.indexOf('?') === -1 ? '?' : '&') + 'pretty=1',
+      dataType: 'text',
+      success: function (data) {
+        var json = formatJSON(data.replace(/[?&]pretty=1/g, ''));
+        $modal.find('pre').html(json.replace(/[?&]format=apibrowser/g, ''));
+        $('a[href^="/"]').click(function (e) {
+          processModal($(this).attr('href'));
+          e.preventDefault();
+        });
+        $modal.modal('show');
+      }
+    });
+  }
+
+  $('#api button').click(function () {
+    processModal($(this).parent().contents().last().text());
+  });
+
+  $modal.modal({show: false});
+*/
 });
