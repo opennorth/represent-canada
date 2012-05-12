3889236c905f5ba3a9fb27d090b6ae7da1f799c3
James McKinney <james@slashpoundbang.com>
2012-05-12 15:25:27 -0400

    make scroll a bit less jumpy

--- finder/static/js/api.js
+++ finder/static/js/api.js
@@ -1,41 +1,24 @@
 jQuery(function ($) {
   if ($('#api').length) {
-    $('body').scrollspy({target: '.subnav', offset: 55}); // 1 more than full height of subnav
-
-    // http://twitter.github.com/bootstrap/assets/js/application.js
     var $window = $(window),
         $subnav = $('.subnav'),
-        offset = $('.subnav').length && $('.subnav').offset().top,
-        fixed = false;
+        offset = $('.subnav').length && $('.subnav').offset().top;
 
-    function processScroll() {
-      var scrollTop = $window.scrollTop()
-      if (scrollTop >= offset && !fixed) {
-        fixed = true;
-        $subnav.addClass('subnav-fixed');
-      } else if (scrollTop <= offset && fixed) {
-        fixed = false;
-        $subnav.removeClass('subnav-fixed');
-      }
-    }
-    processScroll();
+    $('body').scrollspy({target: '.subnav'});
 
-    $window.on('scroll', processScroll);
-
-    $subnav.on('click', function () {
-      if (!fixed) {
-        setTimeout(function () {
-          $window.scrollTop($window.scrollTop() - 54); // full height of subnav
-        }, 10);
-      }
-    });
-
-    $('.subnav').localScroll({
+    $subnav.localScroll({
       axis : 'y',
       duration : 500,
       easing : 'easeInOutExpo',
       hash : true
     });
+
+    function processScroll() {
+      $subnav.toggleClass('subnav-fixed', $window.scrollTop() >= offset);
+    }
+    processScroll();
+
+    $window.on('scroll', processScroll);
   }
 
   $('#apibrowser pre').html(formatJSON($('#apibrowser pre').text()));
