b1146c2fd07439b52a317045aa27b4672827d3e8
James McKinney <james@slashpoundbang.com>
2014-04-18 16:28:28 -0400

    fix api docs

--- finder/static/js/api.js
+++ finder/static/js/api.js
@@ -1,3 +1,7 @@
 $(function () {
-  $('#apibrowser pre').html(formatJSON($('#apibrowser pre').text()));
+  var $apibrowser = $('#apibrowser');
+  if ($apibrowser.length) {
+    var $pre = $apibrowser.find('pre');
+    $pre.html(formatJSON($pre.text()));
+  }
 });
