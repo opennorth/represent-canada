48df65665184a05e52e20392987cab52b34b3891
James McKinney <james@slashpoundbang.com>
2014-11-28 23:07:29 -0500

    make deploy scripts match production more closely

--- deploy-samples/gunicorn_settings.py
+++ deploy-samples/gunicorn_settings.py
@@ -1,19 +1,15 @@
-import os
+import os.path
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-try:
-    NUM_CPUS = os.sysconf('SC_NPROCESSORS_ONLN')
-except:
-    NUM_CPUS = 2
+workers = 5
 
-bind = '127.0.0.1:10101'
+worker_class = 'eventlet'
 
-workers = (NUM_CPUS * 2) + 1
+bind = '127.0.0.1:59000'
 
 pidfile = os.path.join(BASE_DIR, 'gunicorn.pid')
 
-worker_class = 'gevent'
-logfile = os.path.join(BASE_DIR, 'logs', 'gunicorn.log')
+errorlog = os.path.join(BASE_DIR, 'logs', 'gunicorn-error.log')
 
-proc_name = 'repdb PROD'
+proc_name = 'represent'
