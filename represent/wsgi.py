5f90845a5fbf3d022fd4ce441e1e8cd8dfd3a794
James McKinney <james@slashpoundbang.com>
2014-11-28 22:46:41 -0500

    match django 1.7 default generated files

--- represent/wsgi.py
+++ represent/wsgi.py
@@ -1,26 +1,14 @@
 """
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+WSGI config for tmp project.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import os
 
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "represent.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Uncomment the following to enable profiling with linesman
-# from linesman.middleware import ProfilingMiddleware
-# application = ProfilingMiddleware(application)
