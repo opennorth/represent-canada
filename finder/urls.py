adc47c61c2546f65db3542ba656079e6c219fef3
James McKinney <james@slashpoundbang.com>
2017-02-13 17:54:41 -0500

    Update jsi18n for Django 1.9

--- finder/urls.py
+++ finder/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
-from django.views.i18n import JavaScriptCatalog
+# @see https://docs.djangoproject.com/en/1.10/topics/i18n/translation/
+from django.views.i18n import javascript_catalog
 
 from finder import views
 
+js_info_dict = {
+    'packages': ('finder',),
+}
+
 urlpatterns = [
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['finder']), name='javascript-catalog'),
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='javascript-catalog'),
     url(r'^setlang/$', views.set_language, name='set_language'),
     url(r'^api/$', views.api, name='apidoc'),
     url(r'^data/$', views.data, name='data'),
